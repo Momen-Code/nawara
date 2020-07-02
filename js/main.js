3b8496e392d1937bbff252283cfbfd6fdb38bba7
Momen-Code <59899828+Momen-Code@users.noreply.github.com>
2020-07-01 18:03:17 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+
+
+
+
+
+  var swiper2 = new Swiper('.swiper-container2', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination2',
+      clickable: true,
+    },
+  });
